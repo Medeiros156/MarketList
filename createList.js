f27c2a37119102d4f6b07123101d1af15be4b321
Medeiros156 <101615020+Medeiros156@users.noreply.github.com>
2022-11-21 19:03:59 -0300

    first commit

---
+++ createList.js
@@ -0,0 +1,52 @@
+var listSpeech = []
+
+function addToList(speech) {
+    console.log(speech[0]);
+    var url = catchImage(speech[0])
+
+    speech.forEach(e => {
+        listBox.innerHTML += `<div style=background-image:url(${url})> ${e} </div>`
+    });
+
+    listSpeech = [...listSpeech, ...speech]
+    
+    console.log(listSpeech);
+}
+
+function catchImage(keyWord) {
+    const settingsImage = {
+        "async": true,
+        "crossDomain": true,
+        "url": `https://api.unsplash.com/search/photos?page=10&query=${keyWord}`,
+        "method": "GET",
+        "headers": {
+            "Authorization": "Client-ID lt01JJdVhTVWIrFUryd22BhAf7x99UZtMfWpPFt_joU"
+        }
+    }
+
+
+    $.ajax(settingsImage).done(function (response) {
+        console.log(response);
+
+        const data = response.results;
+        console.log(data);
+        createImage(data, keyWord)
+        
+
+        return data
+    });
+}
+
+function createImage(data, keyWord) {
+    const listaUrl = []
+    console.log(data);
+    for (let n = 0; n < data.length; n++) {
+        let url = data[n].urls.raw
+        listaUrl.push(url);
+        
+    }
+    
+
+    ls.setItem(`imagesUrl-${keyWord}`, JSON.stringify(listaUrl))
+     
+}
---
+++ script.js
@@ -0,0 +1,66 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || window.webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+
+
+var recognition = new SpeechRecognition();
+if (SpeechGrammarList) {
+  // SpeechGrammarList is not currently available in Safari, and does not have any effect in any other browser.
+  // This code is provided as a demonstration of possible capability. You may choose not to use it.
+  var speechRecognitionList = new SpeechGrammarList();
+  var grammar = '#JSGF V1.0;' ;
+  speechRecognitionList.addFromString(grammar, 1);
+  recognition.grammars = speechRecognitionList;
+}
+recognition.continuous = false;
+recognition.lang = 'pt-BR';
+recognition.interimResults = false;
+recognition.maxAlternatives = 10;
+
+var diagnostic = document.querySelector('.output');
+
+var hints = document.querySelector('.hints');
+var listBox = document.querySelector('.listBox');
+
+
+document.body.onclick = function() {
+  recognition.start();
+  console.log('Ready to receive a command.');
+  
+}
+
+recognition.onresult = function(event) {
+  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+  // It has a getter so it can be accessed like an array
+  // The first [0] returns the SpeechRecognitionResult at the last position.
+  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+  // These also have getters so they can be accessed like arrays.
+  // The second [0] returns the SpeechRecognitionAlternative at position 0.
+  // We then return the transcript property of the SpeechRecognitionAlternative object
+  var data = event.results[0][0].transcript;
+  var speech = data.split(" ");
+  diagnostic.textContent = 'Result received: ' + speech + '.';
+  console.log('Confidence: ' + event.results[0][0].confidence);
+  console.log(event);
+  console.log(data);
+  console.log(speech);
+
+  
+  addToList(speech)
+  /* createList(speechList) */
+  
+}
+
+recognition.onspeechend = function() {
+  recognition.stop();
+}
+
+recognition.onnomatch = function(event) {
+  diagnostic.textContent = "Didn't understand";
+}
+
+recognition.onerror = function(event) {
+  diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+}
